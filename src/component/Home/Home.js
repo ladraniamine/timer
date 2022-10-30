3bb51edf450233525eb799821b4baba6e8473fe0
Your Name <ladraniful10@gmail.com>
2022-10-30 20:37:05 +0100

    add the responsive navbar and the home page

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -18,57 +18,61 @@ const [second ,setsecond] = useState(0)
       setminute(Math.floor((datedif % (1000*60*60))/(1000*60)))
       setsecond(Math.floor((datedif % (1000*60 ))/(1000)))
   },1000)
+
+let paper_image = "absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]   w-full h-full z-0";
+let time_grid_comtainer = "col-span-3 p-0 lg:p-3";
+let count_nom = "text-md lg:text-xl xl:text-2xl font-bold text-center";
+let count_string = "text-md lg:text-xl xl:text-2xl font-bold text-center";
+
   return (
     <section className='home mt-0'>
-          {/*------------------------------- BACKGROUND IMAGE------------------------------------------------- */}
-          <img className='absolute -z-50 w-full top-0' src={require('../../images/grid.png')}/>
           {/*-------------------------------- IDEATECH HEADER -------------------------------------------------*/}
           <h2 className='text-center mt-40 text-sm md:text-lg  lg:text-2xl font-medium'>ideatech</h2>
           {/*-------------------------------- TITLE OF EVENT --------------------------------------------------*/}
-          <div className='mx-auto mt-5 w-1/2'>
+          <div className='mx-auto mt-5 w-[80%] md:w-[60%] lg:w-1/2'>
             <img className='w-full' src={require("../../images/title.png")}   alt='' />
           </div>
           {/*----------------------------------- BUTTON -------------------------------------------------------*/}
-          <a className='bg-black text-white p-2 block w-1/6 m-auto mt-4 rounded-xl font-meduim cursor-pointer hover:bg-transparent hover:text-black border-solid border border-black'>register now</a>
+          <button className='bg-black text-white p-2 block w-1/5 lg:w-1/6 m-auto mt-4 rounded-xl font-meduim cursor-pointer hover:bg-transparent hover:text-black border-solid border border-black text-[11px] md:text-sm '>register now</button>
       {/*---------------------------------------  TIMER------------------------------------------------------- */}
-      <div className='w-[52%] h-[25vh] m-auto mt-10 grid grid-cols-12'>
+      <div className='w-[400px] h-[100px] lg:w-[52%] lg:h-[25vh] m-auto mt-10 grid grid-cols-12'>
         {/*-------------------------------------- DAYES -------------------------------------------------------*/}
-        <div className=' col-span-3 p-3'>
+        <div className={time_grid_comtainer}>
           <div className='w-full h-full   relative'>
-            <img className='absolute top-0 w-full h-full z-0' src={require("../../images/yellow-paper.png")} />
+            <img className={paper_image} src={require("../../images/yellow-paper.png")} />
             <div className='absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] w-1/2 h-1/2 '>
-              <p className='text-2xl lg:text-2xl font-bold text-center'>{day<10 ? "0"+day:day}</p>
-              <p className='text-2xl text-center font-bold'>days</p>
+              <p className={count_nom}>{day<10 ? "0"+day:day}</p>
+              <p className={count_string}>days</p>
             </div>
           </div>
         </div>
         {/*-------------------------------------- HOURS------------------------------------------------------- */}
-        <div className=' col-span-3 p-3 '>
+        <div className={time_grid_comtainer}>
           <div className='w-full h-full  relative'>
-            <img className='absolute top-0 w-full h-full z-0' src={require("../../images/yellow-paper.png")} />
+            <img className={paper_image} src={require("../../images/yellow-paper.png")} />
             <div className='absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] w-1/2 h-1/2 '>
-              <p className='text-2xl font-bold text-center'>{hour<10 ? "0"+hour:hour}</p>
-              <p className='text-center  text-2xl font-bold'>hours</p>
+              <p className={count_nom}>{hour<10 ? "0"+hour:hour}</p>
+              <p className={count_string}>hours</p>
             </div>
           </div>
         </div>
         {/*------------------------------------- MINUTES------------------------------------------------------ */}
-        <div className=' col-span-3 p-3 '>
+        <div className={time_grid_comtainer}>
           <div className='w-full h-full  relative'>
-            <img className='absolute top-0 w-full h-full z-0' src={require("../../images/yellow-paper.png")} />
+            <img className={paper_image} src={require("../../images/yellow-paper.png")} />
             <div className='absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] w-1/2 h-1/2 '>
-              <p className='text-2xl font-bold text-center'>{minute<10 ? "0"+minute:minute}</p>
-              <p className='text-center  text-2xl font-bold'>mins</p>
+              <p className={count_nom}>{minute<10 ? "0"+minute:minute}</p>
+              <p className={count_string}>mins</p>
             </div>
           </div>
         </div>
         {/*------------------------------------- SECONDS ------------------------------------------------------*/}
-        <div className=' col-span-3 p-3 '>
+        <div className={time_grid_comtainer}>
           <div className='w-full h-full  relative'>
-            <img className='absolute top-0 w-full h-full z-0' src={require("../../images/yellow-paper.png")} />
+            <img className={paper_image} src={require("../../images/yellow-paper.png")} />
             <div className='absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] w-1/2 h-1/2 '>
-              <p className='text-2xl font-bold text-center'>{second<10 ? "0"+second:second}</p>
-              <p className='text-center  text-2xl font-bold'>secs</p>
+              <p className={count_nom}>{second<10 ? "0"+second:second}</p>
+              <p className={count_string}>secs</p>
             </div>
           </div>
         </div>
